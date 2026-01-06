d96e07d433789ece3b86e381d1a75561efde9e46
Thai Bui <quangthai87@gmail.com>
2026-01-06 11:53:17 +0700

    ok

--- rtsp.py
+++ rtsp.py
@@ -1,16 +1,15 @@
 import os
-from webcam_onnx import main
 
-os.environ.setdefault("SRC", "rtsp")
-os.environ.setdefault("RTSP_URL", "")
+os.environ["SRC"] = "rtsp"
+os.environ["RTSP_URL"] = "rtsp://192.168.50.2:8554/mac"
 os.environ.setdefault("RTSP_CODEC", "h264")
 os.environ.setdefault("RTSP_LATENCY", "0")
 
 os.environ.setdefault("ONNX_WEIGHTS", "./model/LP_detector_nano_61.onnx")
-
-os.environ.setdefault("SHOW", "1")   # SSH -> SHOW=0
+os.environ.setdefault("SHOW", "1")
+os.environ.setdefault("IMG_SIZE", "640")
 os.environ.setdefault("CONF", "0.25")
 os.environ.setdefault("IOU", "0.45")
-os.environ.setdefault("IMG_SIZE", "640")
 
+from webcam_onnx import main   # <-- IMPORT sau khi set env
 main()
